b47147b6fd59fd8e64f531b9df68affd593612f6
Dóczy Kristóf <KDoczy@cetin.hu>
2024-12-05 10:35:11 +0100

    URL versioning

--- CompanyEmployees.Presentation/Controllers/CompaniesV2Controller.cs
+++ CompanyEmployees.Presentation/Controllers/CompaniesV2Controller.cs
@@ -4,7 +4,7 @@ using Service.Contracts;
 namespace CompanyEmployees.Presentation.Controllers
 {
     [ApiVersion("2.0")]
-    [Route("api/companies")]
+    [Route("api/{v:apiversion}/companies")]
     [ApiController]
     public class CompaniesV2Controller : ControllerBase
     {
@@ -17,7 +17,9 @@ namespace CompanyEmployees.Presentation.Controllers
         {
             var companies = await _service.CompanyService.GetAllCompaniesAsync(trackChanges: false);
 
-            return Ok(companies);
+            var companiesV2 = companies.Select(x => $"{x.Name} V2");
+
+            return Ok(companiesV2);
         }
     }
 }
