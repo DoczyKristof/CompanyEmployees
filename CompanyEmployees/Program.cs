4818962c476de5e81cc14defdd7dca11a871b40d
Dóczy Kristóf <KDoczy@cetin.hu>
2024-12-05 10:29:56 +0100

    QueryString versioning

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -52,6 +52,7 @@ builder.Services.ConfigureServiceManager();
 builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddCustomMediaTypes();
+builder.Services.ConfigureVersioning();
 
 var app = builder.Build();
 
